a57df18060ef500349171ad291ec0acbaef23009
shift0x <88104243+llfiji@users.noreply.github.com>
2024-07-25 12:37:07 -0400

    updating commit username

--- api/quote.js
+++ api/quote.js
@@ -3,8 +3,11 @@ import { SwapSDK } from "@chainflip/sdk/swap";
 export async function GET(request) {
     const sdk = new SwapSDK();
     const assets = await sdk.getAssets()
+    const body = {
+        assets
+    }
 
-    return new Response(JSON.stringify(assets),  {
+    return new Response(JSON.stringify(body),  {
 		status: 200,
 		headers: {
 			'content-type': 'application/json'
